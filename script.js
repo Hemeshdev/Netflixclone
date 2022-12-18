7eeb33edbe707b43f458d01e1f0a4282691f72fb
Hemeshdev <animinihemesh@gmail.com>
2022-12-18 23:23:58 +0530

    added all files to gitHub

---
+++ script.js
@@ -0,0 +1,76 @@
+//first question
+function answer1() {
+    document.getElementById("answer1").innerHTML = "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.<br><br>You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price.There's always something new to discover, and new TV shows and movies are added every week!";
+    document.getElementById("answer1").style.padding = "0.8em 2.2em 0.8em 1.2em";
+}
+function answer1r(){
+    document.getElementById("answer1").innerHTML = "";
+    document.getElementById("answer1").style.padding = "0px";
+}
+document.getElementById("last1").addEventListener("mouseover", answer1);
+document.getElementById("last1").addEventListener("mouseout", answer1r);
+
+//second question
+
+function answer2(){
+    document.getElementById("answer2").innerHTML = "Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹ 149 to ₹ 649 a month. No extra costs, no contracts.";
+    document.getElementById("answer2").style.padding = "0.8em 2.2em 0.8em 1.2em";
+}
+function answer2r(){
+    document.getElementById("answer2").innerHTML = "";
+    document.getElementById("answer2").style.padding = "0px";
+}
+document.getElementById("last2").addEventListener("mouseover", answer2);
+document.getElementById("last2").addEventListener("mouseout", answer2r);
+
+//third question
+
+function answer3(){
+    document.getElementById("answer3").innerHTML = "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.<br><br>You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.";
+    document.getElementById("answer3").style.padding = "0.8em 2.2em 0.8em 1.2em";
+}
+function answer3r(){
+    document.getElementById("answer3").innerHTML = "";
+    document.getElementById("answer3").style.padding = "0px";
+}
+document.getElementById("last3").addEventListener("mouseover", answer3);
+document.getElementById("last3").addEventListener("mouseout", answer3r);
+
+//fourth question
+
+function answer4(){
+    document.getElementById("answer4").innerHTML = "Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.";
+    document.getElementById("answer4").style.padding = "0.8em 2.2em 0.8em 1.2em";
+}
+function answer4r(){
+    document.getElementById("answer4").innerHTML = "";
+    document.getElementById("answer4").style.padding = "0px";
+}
+document.getElementById("last4").addEventListener("mouseover", answer4);
+document.getElementById("last4").addEventListener("mouseout", answer4r);
+
+//5th question
+
+function answer5(){
+    document.getElementById("answer5").innerHTML = "Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.";
+    document.getElementById("answer5").style.padding = "0.8em 2.2em 0.8em 1.2em";
+}
+function answer5r(){
+    document.getElementById("answer5").innerHTML = "";
+    document.getElementById("answer5").style.padding = "0px";
+}
+document.getElementById("last5").addEventListener("mouseover", answer5);
+document.getElementById("last5").addEventListener("mouseout", answer5r);
+
+//sixth question
+
+function answer6(){
+    document.getElementById("answer6").innerHTML = "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.<br><br>Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.";
+    document.getElementById("answer6").style.padding = "0.8em 2.2em 0.8em 1.2em";
+}
+function answer6r(){
+    document.getElementById("answer6").innerHTML = "";
+    document.getElementById("answer6").style.padding = "0px";
+}
+document.getElementById("last6").addEventListener("mouseover", answer6);
+document.getElementById("last6").addEventListener("mouseout", answer6r);
